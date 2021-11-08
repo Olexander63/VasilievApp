ac40a5e7cf7f580a48c4845e353c467ce771c21c
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-08 19:25:33 +0200

    08/11/21

---
+++ Destructor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Destructor
+{
+    
+    //Клас с конструктором и деструктором:
+    class MyClass
+    {
+        //Закрытое текстоволе поле
+        private string name;
+        //Контсруктор:
+        public MyClass(string txt)
+        {
+            //Присваивание значения полю:
+            name = txt;
+            //Отображение сообщения:
+            Console.WriteLine("Создан обьект \"{0}\"",name);
+        }
+        //Деструктор:
+        ~MyClass()
+        {
+            //Отображение сообщения:
+            Console.WriteLine("Удален обьект \'{0}\'",name);
+        }
+    }
+    
+    // Главный метод:
+    class DestructorDemo
+    {   
+        // Статический метод:
+        static void maker( string txt)
+        {
+            // Создание анонимного обьекта:
+            new MyClass(txt);
+        }
+        
+        // Главный метод:
+        public static void Main(string[] args)
+        {
+            // Создание обьекта:
+            MyClass A = new MyClass("Первый");
+            
+            // Создание анонимного обьекта:
+            new MyClass("Второй");
+            
+            //Новый обьект:
+            A = new MyClass("Третий");
+            
+            //Вызов статического метода:
+            maker("Четвертый");
+            
+            // Новый обьект:
+            A = new MyClass("Пятый");
+        }
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Static
+{
+    
+    //Клас со статическим полем и методом:
+    class MyClass
+    {
+        //  Статическое поле
+        public static int code = 100;
+        
+        // Статический й метод:
+        public static void show()
+        {
+            Console.WriteLine("Статическое поле: " + code);
+        }
+    }
+    class StaticDemo
+    {
+        public static void Main()
+        {
+            // Вызов статического метода:
+            MyClass.show();
+            // Обращение к статическому полю:
+            MyClass.code = 200;
+            // Вызов статического метода:
+            MyClass.show();
+        }
+    }
+}
