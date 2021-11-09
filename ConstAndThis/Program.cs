bfd173d9835e25a810d951dc2e3ced7667cafed9
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-09 15:06:54 +0200

    09.11.21

---
+++ ConstAndThis/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConstAndThis
+{   
+    // Клас с перегрузкой конструкторов:
+    class MyClass
+    {
+        //Целочисленнные поля:
+        public int alpha;
+        public int bravo;
+        // Конструктор с одним аргументом:
+        public MyClass(int a)
+        {
+            // Сообщение в консольном окне:
+            Console.WriteLine("Конструктор с одим аргументом");
+            // Значения полей:
+            alpha = a;
+            bravo = alpha;
+            // Отображение значенияполей:
+            Console.WriteLine("Оба поля равны" +alpha);
+        }
+            
+        // Конструктор с жвумя аргументами:
+        
+        public MyClass(int a, int b) : this(a)
+        {
+            // Сообщение в консольном окне:
+            Console.WriteLine("Конструктор с двумя аргументами ");
+             //  Значение второго поля:
+             bravo = b;
+             // Отображение значений полей:
+             Console.WriteLine("Поля" +alpha+" и " + bravo);
+        }
+        
+        // Конструктор без аргументов:
+        public MyClass() : this(400, 500)
+        {
+            // Сообщение в консольном окне:
+            Console.WriteLine(" Конструктор без аргументов");
+            // отображение значений полей:
+            Console.WriteLine("Значения "+alpha+" и "+bravo);
+        }
+    }
+    // Клас с главгым методлм:
+    
+    class ConstrAndThis
+    {   // Главный метод:
+        public static void Main()
+        {
+             // Вызовв конструктора с одним аргументом:
+             MyClass A = new MyClass(100);
+             Console.WriteLine();
+             // Вызов конструктора с двумя аргументами:
+             MyClass B = new MyClass(200, 300);
+             Console.WriteLine();
+             // Вызов конструктора без аргументов:
+             MyClass C = new MyClass();
+
+        }
+    }
+}
---
+++ UsingThis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace UsingThis
+{
+    class MyClass
+    {
+        // Закрытое целочисленое поле:
+        private int code;
+        // Открытый метод:
+        public int get()
+        {
+            // Использовано ключовое слово this:
+            return this.code;
+        }
+        
+        // Открытый метод:
+        public void set(int code)
+        {
+            // Испольовано ключевое слово this:
+            this.code = code;
+        }
+        // Конструктор:
+        public MyClass(int code)
+        {
+            this.code = code;
+            // Использование ключевое слово this:
+            Console.WriteLine("Создан обьект: " + this.get());
+        }
+    }
+        //Клас с главным методом:
+        
+    class usingThis
+    {   
+        //Главный метод:
+        
+        public static void Main(string[] args)
+        {
+            //Создание обьекта:
+            MyClass obj = new MyClass(100);
+            // Присвание значения полю:
+            obj.set(200);
+            // Проверка значения поля:
+            Console.WriteLine("Новое значение: " + obj.get());
+
+        }
+    }
+}
