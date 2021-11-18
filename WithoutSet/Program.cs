7fdfb99b913801e689f4b42fbd41b34483792fa6
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-18 06:28:07 +0200

    MoreProps

---
+++ WithoutSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WithoutSet
+{
+    class MyClass
+    {
+        public int first;
+        public int last;
+
+        public MyClass(int a, int b)
+        {
+            first = a;
+            last = b;
+        }
+        public int sum
+        {
+            get { return first + last; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта:
+            MyClass obj = new MyClass(1, 9);
+            // Проверка значения свойства:
+            Console.WriteLine("Сумма полей: " + obj.sum);
+            // Присваивание значения полю:
+            obj.first = 6;
+            // Проверка значения свойства:
+            Console.WriteLine("Сумма полей: " + obj.sum);
+            // Присваивание значения полю:
+            obj.last = 2;
+            // Проверка значения свойства:
+            Console.WriteLine("Сумма полей: " + obj.sum);
+        }
+    }
+}
