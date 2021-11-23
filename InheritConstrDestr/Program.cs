6a7158101351a53384d84ea8d7db215c75786a36
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-23 09:12:08 +0200

    InheritConstrDestr

---
+++ InheritConstrDestr/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InheritConstrDestr
+{
+    class Alpha
+    {
+        public Alpha()
+        {
+            Console.WriteLine("Конструкторклассса Alpha");
+        }
+
+        ~Alpha()
+        {
+            Console.WriteLine("Деструктор класса Alpha");
+        }
+    }
+
+    class Bravo: Alpha
+    {
+        public Bravo() : base()
+        {
+            Console.WriteLine("Конструктор класа Bravo");
+        }
+    }
+
+    class  Charlie: Bravo
+    {
+        public Charlie() : base()
+        {
+            Console.WriteLine("Конструктор класса Charlie");
+        }
+
+        ~Charlie()
+        {
+            Console.WriteLine("Деструктор класса Charlie");
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Charlie();
+        }
+    }
+}
