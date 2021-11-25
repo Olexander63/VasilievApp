22ce0ec2579a70d74043c1fd3e6f03581e3288ba
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-25 12:54:46 +0200

    OverloadAndOverride

---
+++ OverloadAndOverride/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OverloadAndOverride
+{
+    class Alpha
+    {
+        public int alpha;
+
+        public void set()
+        {
+            alpha = 10;
+            Console.WriteLine("Alpha (без аргументов): {0} ", alpha);
+        }
+
+        public virtual void set(int n)
+        {
+            alpha = n;
+            Console.WriteLine("Alpha (один аргумент): {0}", alpha);
+        }
+    }
+
+    class Bravo: Alpha
+    {
+        public int bravo;
+
+        public override void set(int n)
+        {
+            alpha = n;
+            bravo = alpha;
+            Console.WriteLine("Bravo (один аргумент): {0}  и {1} ", alpha, bravo);
+        }
+
+        public void set(int m, int n)
+        {
+            alpha = m;
+            bravo = n;
+            Console.WriteLine("Bravo (два аргументами) ", alpha, bravo);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Alpha A = new Alpha();
+            A.set();
+            A.set(20);
+            Console.WriteLine();
+
+            Bravo B = new Bravo();
+            B.set();
+            B.set(30);
+            B.set(40,50);
+        }
+    }
+}
