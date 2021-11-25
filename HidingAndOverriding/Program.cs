21dc60443fefa00e4404034f669a8a4c2a147f7f
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-25 12:04:13 +0200

    HidingAndOverriding

---
+++ HidingAndOverriding/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace HidingAndOverriding
+{
+    class Alpha
+    {
+        public void hello()
+        {
+            Console.WriteLine("Метод hello() из класса Alpha");
+        }
+
+        public virtual void hi()
+        {
+            Console.WriteLine("Метод hi() из класса Alpha");
+        }
+
+        public void show()
+        {
+            hello();
+            hi();
+            Console.WriteLine();
+        }
+    }
+
+    class Bravo: Alpha
+        {
+            new public void hello()
+            {
+                Console.WriteLine("Метод hello() из класса Bravo");
+            }
+        }
+    
+        internal class Program
+        {
+            public static void Main(string[] args)
+            {
+                // Создание объекта базового класса:
+                Alpha A=new Alpha();
+                Console.WriteLine("Выполнение команды A.show():");
+                // Вызов метода:
+                A.show();
+                // Создание объекта производного класса:
+                Bravo B=new Bravo();
+                Console.WriteLine("Выполнение команды B.hello():");
+                // Вызов замещающего метода из производного класса:
+                B.hello();
+                Console.WriteLine("Выполнение команды B.hi():");
+                // Вызов переопределенного метода:
+                B.hi();
+                Console.WriteLine("Выполнение команды B.show():");
+                // Вызов унаследованного метода:
+                B.show();
+                Console.WriteLine("После выполнения команды A=B");
+                // Объектной переменной базового класса присваивается
+                // ссылка на объект производного класса:
+                A=B;
+                Console.WriteLine("Выполнение команды A.hello():");
+                // Вызов замещаемого метода:
+                A.hello();
+                Console.WriteLine("Выполнение команды A.hi():");
+                // Вызов переопределенного метода:
+                A.hi();
+                Console.WriteLine("Выполнение команды A.show():");
+                // Вызов унаследованного метода:
+                A.show();
+            }
+        }
+}
