9a018e579a5d17ecf893dbc0629c97ee7a509681
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-30 14:30:18 +0200

    ExplInterface

---
+++ ExplInterface/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ExplInterface
+{
+    abstract class Base
+    {
+        public abstract char symbol
+        {
+            get;
+        }
+
+        public abstract int this[int k]
+        {
+            get;
+        }
+
+        public abstract void show();
+    }
+
+    interface First
+    {
+        char symbol
+        {
+            get;
+        }
+
+        int this[int k]
+        {
+            get;
+        }
+
+        void show();
+    }
+
+    interface Second
+    {
+        char symbol
+        {
+            get;
+        }
+
+        int this[int k]
+        {
+            get;
+        }
+
+        void show();
+    }
+
+    class MyClass:Base,First,Second
+    {
+        private char smb;
+
+        public MyClass(char s) : base()
+        {
+            smb = s;
+        }
+
+        public override char symbol
+        {
+            get
+            {
+                return smb;
+            }
+        }
+
+        char First.symbol
+        {
+            get
+            {
+                return (char) (smb + 1);
+            }
+        }
+
+        public override int this[int k]
+        {
+            get
+            {
+                return smb + k;
+            }
+        }
+
+        int Second.this[int k]
+        {
+            get
+            {
+                return smb - k;
+            }
+        }
+
+        public override void show()
+        {
+            Console.WriteLine(" Базовый класс Base: \t\'{0}\'",symbol);
+        }
+
+        void First.show()
+        {
+            Console.WriteLine("Interface First: \t\'{0}\'",symbol);
+        }
+        void Second.show()
+        {
+            Console.WriteLine("Interface Second: \t\'{0}\'",symbol);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass obj = new MyClass('A');
+            First A = obj;
+            Second B = obj;
+            obj.show();
+            A.show();
+            B.show();
+            Console.WriteLine(" obj.symbol=\'{0}\'", obj.symbol);
+            Console.WriteLine("  A.symbol=\'{0}\'", A.symbol);
+            Console.WriteLine("  B.symbol=\'{0}\'", B.symbol);
+
+            Console.WriteLine("obj[10] = {0}", obj[10]);
+            Console.WriteLine(" A[10] = {0}", A[10]);
+            Console.WriteLine(" B[10] = {0}", B[10]);
+        }
+    }
+}
---
+++ InterfaceVar/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace InterfaceVar
+{
+    
+    
+    interface MyInterface
+    {
+        char getChar(int n);
+        char this[int k]
+        {
+            get;
+        }
+    }
+
+    class Alpha:MyInterface
+    {
+        private char symb;
+
+        public Alpha(char s)
+        {
+            symb = s;
+        }
+
+        public char getChar(int n)
+        {
+            return (char) (symb + n);
+        }
+
+        public char this[int k]
+        {
+            get
+            {
+                return getChar(k);
+            }
+        }
+    }
+
+    class Bravo:MyInterface
+    {
+        private string text;
+
+        public Bravo(string t)
+        {
+            text = t;
+        }
+
+        public char getChar(int k)
+        {
+            return text[k % text.Length];
+        }
+
+        public char this[int k]
+        {
+            get
+            {
+                return getChar(k);
+            }
+        }
+    }
+   class Program
+    {
+        public static void Main(string[] args)
+        {
+            int m = 5;
+            MyInterface R;
+            R = new Alpha('F');
+            Console.WriteLine("Символы от \'{0}\' до \'{1}\':", R.getChar(-m),R.getChar(m));
+            for (int i = -m; i <=m; i++)
+            {
+                Console.Write("|" + R[i]);
+            }
+
+            Console.WriteLine("|");
+            R = new Bravo("bravo");
+            Console.WriteLine("Символы от \'{0}\' до \'{1}\':",R.getChar(0),R.getChar(2*m+1));
+
+            for (int i = 0; i <=2*m+1 ; i++)
+            {
+                Console.Write("|"+R[i]);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
