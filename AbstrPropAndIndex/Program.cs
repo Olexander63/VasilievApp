6f91fa0801f680979343c2f0c31eabf803938d85
Oleksandr Omelchenko <olexander63@gmail.com>
2021-11-30 12:03:47 +0200

    Interface

---
+++ AbstrPropAndIndex/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace AbstrPropAndIndex
+{
+    abstract class Base
+    {
+        public abstract string text
+        {
+            get;
+            set;
+        }
+
+        public abstract char this[int k]
+        {
+            get;
+        }
+
+        public abstract int length
+        {
+            get;
+        }
+    }
+
+    class Alpha:Base
+    {
+        private char[] symbs;
+
+        public Alpha(string t) : base()
+        {
+            text = t;
+        }
+
+        public override string text
+        {
+            get
+            {
+                return new string(symbs);
+            }
+            set
+            {
+                symbs = value.ToCharArray();
+            }
+        }
+
+        public override int length
+        {
+            get
+            {
+                return symbs.Length;
+            }
+        }
+
+        public override char this[int k]
+        {
+            get
+            {
+                return symbs[k];
+            }
+        }
+    }
+
+    class Bravo:Base
+    {
+        private string txt;
+
+        public Bravo(string t) : base()
+        {
+            text = t;
+        }
+
+        public override string text
+        {
+            get
+            {
+                return txt;
+            }
+            set
+            {
+                txt = value;
+            }
+        }
+
+        public override int length
+        {
+            get
+            {
+                return txt.Length;
+            }
+        }
+
+
+        public override char this[int k]
+        {
+            get
+            {
+                return txt[k];
+            }
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Base obj = new Alpha("Alpha");
+            Console.WriteLine(obj.text);
+            obj.text = "Base";
+            for (int k = 0; k < obj.length; k++)
+            {
+                Console.Write("|"+ obj[k]);
+            }
+
+            Console.WriteLine("|");
+            obj = new Bravo("Bravo");
+            for (int k = 0; k < obj.length; k++)
+            {
+                Console.Write("|"+obj[k]);
+            }
+
+            Console.WriteLine("|");
+            obj.text = "Base";
+            Console.WriteLine(obj.text);
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Interface
+{
+    interface MyInterface
+    {
+        void show();
+        void setNum(int n);
+        int getNum();
+
+        int number
+        {
+            get;
+            set;
+        }
+
+        int this[int k]
+        {
+            get;
+        }
+    }
+
+    class MyClass:MyInterface
+    {
+        private int num;
+
+        public MyClass(int n)
+        {
+            number = n;
+            show();
+        }
+
+        public void show()
+        {
+            Console.WriteLine(" Свойство number "+ number);
+        }
+
+        public void setNum(int n)
+        {
+            num = n;
+        }
+
+        public int getNum()
+        {
+            return num;
+        }
+
+        public int number
+        {
+            get
+            {
+                return getNum();
+            }
+            set
+            {
+                setNum(value);
+            }
+        }
+
+        public int this[int k]
+        {
+            get
+            {
+                int r = number;
+                for (int i = 0; i < k; i++)
+                {
+                    r /= 10;
+                }
+
+                return r % 10;
+            }
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int m = 9;
+            MyClass obj = new MyClass(12345);
+            for (int i = 0; i <=m; i++)
+            {
+                Console.Write("|" + obj[m-i]);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
