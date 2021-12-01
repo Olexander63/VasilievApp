b29040f28969b0bb5dba5158f89c752d6e85107e
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-01 14:31:53 +0200

    AnonymMethAsArg

---
+++ AnonymMethAsArg/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnonymMethAsArg
+{
+    delegate char MyDelegate(int n);
+
+    class MyClass
+    {
+        public char symbol;
+        public MyDelegate get;
+
+        public MyClass(char s, MyDelegate md)
+        {
+            symbol = s;
+            get = md;
+        }
+
+        public void set(MyDelegate md)
+        {
+            get = md;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass obj=new MyClass('K',
+                delegate(int n)
+                {
+                    return (char) ('A' + n);
+                }
+            );
+            Console.WriteLine("  \'{0}\'", obj.get(3));
+            obj.set(delegate(int n)
+            {
+                return (char) (obj.symbol + n);
+            }
+                );
+            Console.WriteLine("  \'{0}\'", obj.get(3));
+        }
+    }
+}
