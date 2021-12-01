88b5caf4ebfd87f7c552b12c369d724c4886a2a9
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-01 14:47:50 +0200

    AnonymAsResult

---
+++ AnonymAsResult/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AnonymAsResult
+{
+    delegate int MyDelegate();
+
+    class AnonymAsResult
+    {
+        private static MyDelegate calculate(int n)
+        {
+            int count = 0;
+            return delegate
+            {
+                count += n;
+                return count;
+            };
+        }
+        
+        public static void Main()
+        {
+            MyDelegate next = calculate(1);
+            for (int i = 1; i <=5; i++)
+            {
+                Console.Write(next() +" ");
+            }
+
+            Console.WriteLine();
+            next = calculate(3);
+            for (int i = 1; i <=5; i++)
+            {
+                Console.Write(next() +" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+}
