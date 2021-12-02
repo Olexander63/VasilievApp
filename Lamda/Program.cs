c87d6f0dcf3c47a82265a6534235385143aeb0f0
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-02 11:45:10 +0200

    LambdaAsArg

---
+++ Lamda/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lamda
+{
+    delegate int Alpha(int n);
+
+    delegate void Bravo(string t);
+
+    class MyClass
+    {
+        public int number;
+
+        public MyClass(int n)
+        {
+            number = n;
+        }
+
+        public Alpha method;
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass A = new MyClass(100);
+            MyClass B = new MyClass(200);
+            A.method = (int n) => A.number + n;
+            B.method = (int n) =>
+            {
+                return B.number - n;
+            };
+            int x = 80;
+            Console.WriteLine("A.method({0})= {1}",x,A.method(x));
+            A.number = 300;
+            Console.WriteLine("A.method({0})= {1}",x,A.method(x));
+            Console.WriteLine("B.method({0})= {1}",x,B.method(x));
+            Bravo show;
+            show =t=> Console.WriteLine("Текст: \"{0}\"", t);
+            show = (string t) =>
+            {
+                for (int k = 0; k < t.Length; k++)
+                {
+                    Console.Write("|" + t[k]);
+                }
+
+                Console.WriteLine("|");
+            };
+            show("Bravo");
+        }
+    }
+}
---
+++ LamdaAsArg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LamdaAsArg
+{
+    delegate char MyDelegate(int n);
+
+    class MyClass
+    {
+        public char symbol;
+        public MyDelegate get;
+
+        public MyClass(char s, MyDelegate md)
+        {
+            symbol = s;
+            get = md;
+        }
+
+        public void set(MyDelegate md)
+        {
+            get = md;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass obj = new MyClass('K', n => (char) ('A' + n));
+            Console.WriteLine("Символ: \'{0}\'",obj.get(3));
+            obj.set(n=>(char)(obj.symbol + n));
+            Console.WriteLine("Символ: \'{0}\'",obj.get(3));
+        }
+    }
+}
