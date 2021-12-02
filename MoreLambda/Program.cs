99e0e8e571f1f57a0a5be17d7e528baf2fce250b
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-02 12:10:02 +0200

    MoreLambda

---
+++ MoreLambda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoreLambda
+{
+    class MyClass
+    {
+        private string text;
+
+        public MyClass(string t)
+        {
+            text = t;
+        }
+
+        public char this[int k] => text[k];
+        public int length => text.Length;
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass obj = new MyClass("Alpha");
+            for (int k = 0; k < obj.length; k++)
+            {
+                Console.Write("|"+ obj[k]);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
