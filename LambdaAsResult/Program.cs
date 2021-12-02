8869e11b945bcff35b42bd7e0e04c95531c65fb9
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-02 11:52:36 +0200

    LambdaAsResult

---
+++ LambdaAsResult/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LambdaAsResult
+{
+    delegate int MyDelegate();
+
+    class LambdaAsResultDemo
+    {
+        static MyDelegate calculate(int n)
+        {
+            int cont = 0;
+            return () =>
+            {
+                cont += n;
+                return cont;
+            };
+        }
+        public static void Main(string[] args)
+        {
+            MyDelegate next = calculate(1);
+            for (int i = 1; i <=5; i++)
+            {
+                Console.Write(next()+" ");
+            }
+
+            Console.WriteLine();
+            next = calculate(3);
+            for (int i = 1; i <=5; i++)
+            {
+                Console.Write(next()+" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
