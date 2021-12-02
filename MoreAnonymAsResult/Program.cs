87368aa63b83f3b015a47ea13f71c70264a1495e
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-02 11:36:20 +0200

    Lambda

---
+++ MoreAnonymAsResult/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MoreAnonymAsResult
+{
+    delegate double Powers(double x);
+    internal class Program
+    {
+        static Powers maker(int n)
+        {
+            Powers meth;
+            meth = delegate(double x)
+            {
+                double s = 1;
+                for (int i = 1; i <=n; i++)
+                {
+                    s *= x;
+                }
+
+                return s;
+            };
+            return meth;
+        }
+        public static void Main(string[] args)
+        {
+            Powers sqr = maker(2);
+            Powers cube = maker(3);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("{0,2}:{1,5}{2,5}{3,5}", i, sqr(i),cube(i),maker(4)(i));
+            }
+        }
+    }
+}
