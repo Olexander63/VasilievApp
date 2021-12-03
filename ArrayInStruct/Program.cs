7726a65271b9cf70190208383e937fb2a7c3b660
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-03 13:14:24 +0200

    StructArray

---
+++ ArrayInStruct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ArrayInStruct
+{
+    struct MyStruct
+    {
+        public  char[] symbs;
+
+        public void show()
+        {
+            for (int k = 0; k < symbs.Length; k++)
+            {
+                Console.Write("|"+ symbs[k]);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyStruct A, B;
+            A.symbs = new char[7];
+            for (int k = 0; k < A.symbs.Length; k++)
+            {
+                A.symbs[k] = (char)
+                ('A' + k);
+            }
+
+            Console.WriteLine("Экземпляр А:");
+            A.show();
+            B = A;
+            Console.WriteLine("Экземпляр B:");
+            B.show();
+            A.symbs[0] = 'X';
+            B.symbs[B.symbs.Length - 1] = 'Y';
+            Console.WriteLine("Экземпляр A:");
+            A.show();
+            Console.WriteLine("Экзепляр B:");
+            B.show();
+        }
+    }
+}
---
+++ StructArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StructArray
+{
+    struct MyStruct
+    {
+        public int code;
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = 7;
+            MyStruct[] A = new MyStruct[size];
+            for (int k = 0; k < A.Length; k++)
+            {
+                A[k].code = 2 * k + 1;
+                Console.Write("|"+A[k].code);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
