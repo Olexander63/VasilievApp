e7ae4f6b0d4e4a4559558cfa17dd91c63e1b8a67
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-03 12:45:25 +0200

    Struct

---
+++ Struct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Struct
+{
+    struct MyStruct
+    {
+        public int code;
+        public string name;
+
+        public MyStruct(int n, string t)
+        {
+            code = n;
+            name = t;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Поля \"{0}\" и {1}",name,code);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyStruct A;
+            A.code = 100;
+            A.name = "Экземпляр А";
+            A.show();
+            MyStruct B = new MyStruct(200, "Экземпляр B");
+            B.show();
+            A = B;
+            B.code = 300;
+            A.show();
+            B.show();
+            
+        }
+    }
+}
