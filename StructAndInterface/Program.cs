4ca465f3d91bbd75b8b3699e2dd361d6a6b01b92
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-04 14:08:34 +0200

    Pointers

---
+++ StructAndInterface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StructAndInterface
+{
+    interface MyInterface
+    {
+        void set(int n);
+        void show();
+    }
+
+    struct MyStruct:MyInterface
+    {
+        private int code;
+
+        public void set(int n)
+        {
+            code = n;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Числовое поле: "+ code);
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyStruct A = new MyStruct();
+            A.set(100);
+            Console.WriteLine("Экземпляр А:");
+            A.show();
+            MyInterface R;
+            R = A;
+            Console.WriteLine("Переменная R:");
+            R.show();
+            Console.WriteLine("Экземпляр A:");
+            A.show();
+        }
+    }
+}
