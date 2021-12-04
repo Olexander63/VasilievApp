51c3c4e71d051da96c42811082e2e997df908a99
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-04 11:11:33 +0200

    StructAndEvent

---
+++ StructAsResult/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StructAsResult
+{
+    struct MyStruct
+    {
+        public char[] name;
+        public int code;
+
+        public MyStruct(char[] s, int n)
+        {
+            name = s;
+            code = n;
+        }
+
+        public void show()
+        {
+            Console.Write("Массив: ");
+            for (int k = 0; k < name.Length; k++)
+            {
+                Console.Write("|"+name[k]);
+            }
+
+            Console.WriteLine("|\n Числовое поле: {0}",code);
+        }
+    }
+    internal class Program
+    {
+        static MyStruct create(string t, int n)
+        {
+            char[] s = t.ToCharArray();
+            MyStruct R = new MyStruct(s, n);
+            return R;
+        }
+        public static void Main(string[] args)
+        {
+            MyStruct A;
+            A = create("Apha", 100);
+            A.show();
+            create("Bravo",200).show();
+        }
+    }
+}
