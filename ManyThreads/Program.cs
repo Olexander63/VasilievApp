65ad9c6ebed23f12634cc46b5b2502943b8b22eb
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-07 17:05:13 +0200

    ManyThreads

---
+++ ManyThreads/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace ManyThreads
+{
+    class MyClass
+    {
+        private int count;
+        private int time;
+        private string text;
+
+        public void show()
+        {
+            Console.WriteLine(text+": Начало...");
+            for (int i = 1; i <=count ; i++)
+            {
+                Console.WriteLine(text+" -> "+ i);
+                Thread.Sleep(time);
+            }
+
+            Console.WriteLine(text+": Завершение...");
+        }
+
+        public MyClass(int c, int t, string txt)
+        {
+            count = c;
+            time = t;
+            text = txt;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Главный поток: Начало...");
+            MyClass objA = new MyClass(5, 2000, "Поток А");
+            MyClass objB = new MyClass(6, 1200, "Поток B");
+            MyClass objC = new MyClass(8, 1000, "Поток C");
+            Thread A = new Thread(objA.show );
+            Thread B = new Thread(objB.show );
+            Thread С = new Thread(objC .show );
+            A.Start();
+            B.Start();
+            С.Start();
+            Console.WriteLine("Главный поток: Завершение... ");
+        }
+    }
+}
---
+++ Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Thread
+{
+    internal class Program
+    {
+        static void run()
+        {
+            Console.WriteLine("Запускаем дочерний поток");
+            for (int k = 0; k <=5; k++)
+            {
+                Console.WriteLine("Дочерний поток: {0}",(char)('A'+k));
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Дочерний поток завершен");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Запускаем главный поток");
+            System.Threading.Thread mt = new System.Threading.Thread(run);
+            mt.Start();
+            for (int k = 1; k <=5 ; k++)
+            {
+                Console.WriteLine("Главынй поток: "+ k);
+                System.Threading.Thread.Sleep(2000);
+            }
+
+            Console.WriteLine("Главный поток завершен");
+        }
+    }
+}
