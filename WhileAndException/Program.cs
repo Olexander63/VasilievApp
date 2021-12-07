d11273634e9113229784b63f49ff8a87ad8f5c83
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-07 12:19:12 +0200

    WhileAndException

---
+++ WhileAndException/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace WhileAndException
+{
+    class MyClass
+    {
+        private int[] nums;
+
+        public MyClass(int size)
+        {
+            nums = new int[size];
+        }
+
+        public int this[int k]
+        {
+            get
+            {
+                return nums[k];
+            }
+            set
+            {
+                nums[k] = value;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void show(MyClass obj)
+        {
+            int k = 0;
+            try
+            {
+                while (true)
+                {
+                    Console.Write("|" + obj[k]);
+                    k++;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("|");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            MyClass obj = new MyClass(10);
+            int k = 0;
+            try
+            {
+                do
+                {
+                    obj[k] = 2 * k + 1;
+                    k++;
+                } while (true);
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                show(obj);
+            }
+        }
+    }
+}
