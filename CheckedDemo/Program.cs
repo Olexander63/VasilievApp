7aa5cc056e9a92b44a7f67063a19405497058701
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-07 15:08:03 +0200

    CheckedDemo

---
+++ CheckedDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CheckedDemo
+{
+    class MyClass
+    {
+        private byte num;
+
+        public int number
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                try
+                {
+                    checked
+                    {
+                        num = (byte) value;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    num = 255;
+                }
+            }
+        }
+
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.number = 100;
+            Console.WriteLine("Значение свойства: "+ obj.number);
+            obj.number = 300;
+            Console.WriteLine("Значение свойства: "+ obj.number);
+        }
+    }
+}
