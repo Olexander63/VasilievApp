472655920a9cc8de9396c9780dbd22d3b7ea0c0e
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-08 16:49:42 +0200

    FillingArray

---
+++ FillingArray/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+namespace FillingArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = 20;
+            char[] symbs = new char[size];
+            for (int k = 0; k <symbs.Length; k++)
+            {
+                symbs[k] = '*';
+                Console.Write("|"+ symbs[k]);
+            }
+
+            Console.WriteLine("|");
+            int first = 0, second = symbs.Length - 1;
+            Thread A, B;
+            A = new Thread(() =>
+            {
+                char start = 'Я';
+                while (true)
+                {
+                    if (second > first)
+                    {
+                        symbs[second] = start;
+                        start--;
+                        second--;
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        Thread.CurrentThread.Abort();
+                    }
+                }
+            });
+            B = new Thread(() =>
+            {
+                char start = 'A';
+                while (true)
+                {
+                    if (first < second)
+                    {
+                        symbs[first] = start;
+                        start++;
+                        first++;
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        Thread.CurrentThread.Abort();
+                    }
+                }
+            });
+            A.Start();
+            B.Start();
+            if (A.IsAlive) A.Join();
+            if (B.IsAlive) B.Join();
+            for (int k = 0; k < symbs.Length; k++)
+            {
+                Console.Write("|"+symbs[k]);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
---
+++ lock/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Lock
+{
+    class MyClass
+    {
+        public int state = 0;
+    }
+internal class Program
+{
+    static void run(bool type, MyClass obj)
+    {
+        int val, k = 1;
+        Random rnd = new Random();
+        while (k<=5)
+        {
+            val = obj.state;
+            Console.WriteLine("{0,4}: прочитано значение {1,2}", Thread.CurrentThread.Name, val);
+            Thread.Sleep(rnd.Next(1000,3001));
+            if (type) val++;
+            else val--;
+            Console.WriteLine("{0,4}: присвоено значение {1,2}", Thread.CurrentThread.Name, obj.state);
+            k++;
+            // lock (obj)
+            // {
+            //
+            // }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        MyClass obj = new MyClass();
+        Thread up = new Thread(() => run(true, obj));
+        up.Name = "UP";
+        Thread down = new Thread(() => run(false, obj));
+        down.Name = "DOWN";
+        up.Start();
+        down.Start();
+    }
+}
+}
