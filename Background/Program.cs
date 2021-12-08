aa8c7ad901d24313093206a477a5c7517376c6ae
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-08 15:36:33 +0200

    CurrentThread

---
+++ Background/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Background
+{
+    internal class Program
+    {
+        static void run()
+        {
+            Console.WriteLine("Дочерний поток запущен...");
+            int k = 1;
+            while (true)
+            {
+                Console.WriteLine("Дочерний поток: "+ k);
+                k++;
+                Thread.Sleep(2100);
+            }
+        }
+        static void Main()
+        {
+            Console.WriteLine("Главный поток запущен...");
+            Thread t = new Thread(run);
+            t.IsBackground = true;
+            t.Start();
+            for (char s ='A'; s <= 'F'; s++)
+            {
+                Console.WriteLine("Главный поток: " +s);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Главный поток завершен...");
+        }
+    }
+}
---
+++ CurrentThread/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace CurrentThread
+{
+    class Program
+    {
+        static int state = 0;
+        static void run(bool type)
+        {
+            Thread t = Thread.CurrentThread;
+            Console.WriteLine("Поток "+t.Name+" запущен...");
+            while (true)
+            {
+                if (type) state++;
+                else state--;
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main()
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Base";
+            Console.WriteLine("Главный поток {0} запущен... ", t.Name);
+            Console.WriteLine("Начальное значение: {0}",state);
+            Thread up = new Thread(()=>run(true));
+            Thread down = new Thread(() => run((false)));
+            up.Name = "Alpha";
+            up.Priority = ThreadPriority.Highest;
+            down.Name = "Bravo";
+            down.Priority = ThreadPriority.Lowest;
+            up.Start();
+            down.Start();
+            Thread.Sleep(5000);
+            up.Abort();
+            down.Abort();
+            Console.WriteLine("Итоговое значение: {0}",state);
+            Console.WriteLine("Главный поток {0} завершен...",t.Name);
+        }
+    }
+}
