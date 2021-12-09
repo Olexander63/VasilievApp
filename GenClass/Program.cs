f4724ca0be684d46f90473bdb101d7668dd092d2
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-09 17:20:36 +0200

    GenClass

---
+++ GenClass/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GenClass
+{
+    class Alpha<X>
+    {
+        public  X code;
+
+        public Alpha(X a)
+        {
+            code = a;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Поле типа {0}: {1}",typeof(X).Name,code);
+        }
+    }
+
+    class Bravo<X,Y>
+    {
+        public X first;
+        public Y second;
+
+        public Bravo(X a, Y b)
+        {
+            first = a;
+            second = b;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Первое поле типа {0}: {1}",typeof(X).Name,first);
+            Console.WriteLine("Второе поле типа {0}: {1}",typeof(Y).Name,second);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Alpha<int> A = new Alpha<int>(123);
+            Alpha<string> B = new Alpha<string>("object B");
+            Alpha<char> C = new Alpha<char>('C');
+            A.show();
+            B.show();
+            C.show();
+            Console.WriteLine();
+            Bravo<int, char> objA = new Bravo<int, char>(123, 'A');
+            Bravo<string, bool> objB = new Bravo<string, bool>("objB", true);
+            Bravo<char, char> objC = new Bravo<char, char>('B', 'C');
+            objA.show();
+            objB.show();
+            objC.show();
+        }
+    }
+}
---
+++ WhithGenMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WhithGenMethod
+{
+    class MyClass
+    {
+        public string text;
+
+        public MyClass(string t)
+        {
+            text = t;
+        }
+
+        public void show<X>(X arg)
+        {
+            Console.WriteLine(text+": "+ arg);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass A = new MyClass("Alpha");
+            MyClass B = new MyClass("Bravo");
+            A.show(100);
+            B.show(200);
+            A.show('A');
+            B.show<char>('B');
+            B.show<int>('B');
+
+        }
+    }
+}
