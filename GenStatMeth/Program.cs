0d489d14165f02325bbbae89cd904a8df7444277
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-09 15:09:01 +0200

    MoreGenMeth

---
+++ GenStatMeth/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GenStatMeth
+{
+    class MyClass
+    {
+        public int code;
+
+        public MyClass(int n)
+        {
+            code = n;
+        }
+
+        public override string ToString()
+        {
+            return "поле =" + code;
+        }
+
+        public static implicit operator int(MyClass obj)
+        {
+            return obj.code;
+        }
+
+        public static implicit operator MyClass(int n)
+        {
+            return new MyClass(n);
+        }
+    }
+
+    class GenStatMethDemo
+    {
+        static void show<T>(T arg)
+        {
+            Console.WriteLine("Обобщенный параметр: {0}", typeof(T).Name);
+            Console.WriteLine("Значение аргумента:  {0}", arg);
+            Console.WriteLine("-------------------------------");
+        }
+
+        static void Main()
+        {
+            show(100);
+            show<int>(200);
+            show<double>(300);
+            show<MyClass>(400);
+            show('A');
+            show<char>('B');
+            show<int>('C');
+            show("Alpha");
+            MyClass obj = new MyClass(500);
+            show(obj);
+            show<int>(obj);
+        }
+    }
+}
---
+++ MoreGenMeth/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MoreGenMeth
+{
+    internal class Program
+    {
+        static void show<T, U>(T a, U b)
+        {
+            Console.WriteLine("Первый аргумент {0}",a);
+            Console.WriteLine("Второй аргумент {0}",b);
+        }
+        public static void Main(string[] args)
+        {
+            show(100,'A');
+            show('B',"Bravo");
+            show('C','D');
+            show<int,char>('C','D');
+        }
+    }
+}
