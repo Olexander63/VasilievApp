111e669349bc8d5b870c94808e896b7157092e80
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-09 14:58:46 +0200

    GenStatMeth

---
+++ SumCalc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace SumCalc
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double s = 0;
+            Console.WriteLine("Вычисление суммы");
+            Console.WriteLine("Контрольное щначение: {0}",2*Math.E);
+            Thread calc = new Thread(() =>
+            {
+                int n = 1;
+                double q = 1;
+                do
+                {
+                    s += q;
+                    n++;
+                    q = n * n;
+                    for (int k = 1; k <=n ; k++)
+                    {
+                        q /= k;
+                    }
+                    Thread.Sleep(100);
+                } while (true);
+            });
+            calc.Start();
+            Thread.Sleep(1000);
+            calc.Abort();
+            Console.WriteLine("Вычисление значение:{0}",s);
+        }
+    }
+}
