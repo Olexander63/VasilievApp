ee78c37d0a984a01c07c3a073a83ae16c26f2c7b
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-10 20:12:30 +0200

    SimpleFormDemo

---
+++ SimpleForm/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleForm
+{
+    public class SimpleFormDemo
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Form mf = new Form();
+            mf.Text = "Простое окно";
+            mf.Height = 200;
+            mf.Width = 300;
+            Application.Run(mf);
+        }
+    }
+}
