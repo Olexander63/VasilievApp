f4e7ea7cb9ffb6d6f8adb17f46eabe7f90e079fa
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-10 17:44:56 +0200

    MultiGenRest

--- MoreGenRest/Program.cs
+++ MoreGenRest/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace MoreGenRest
+﻿using System;
+
+namespace MoreGenRest
 {
+    class Alpha
+    {
+        public int code;
+
+        public override string ToString()
+        {
+            return "Alpha: " + code;
+        }
+    }
+
+    class Bravo
+    {
+        public string text;
+
+        public override string ToString()
+        {
+            return "Bravo: " + text;
+        }
+    }
+
+    class MyClass<T> where T: new()
+    {
+        public T obj;
+
+        public MyClass()
+        {
+            obj = new T();
+        }
+
+        public void show()
+        {
+            Console.WriteLine(obj);
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
         {
+            MyClass<Alpha> objA = new MyClass<Alpha>();
+            objA.obj.code = 123;
+            objA.show();
+            MyClass<Bravo> objB = new MyClass<Bravo>();
+            objB.obj.text = "text";
+            objB.show();
         }
     }
 }
---
+++ MultiGenRest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace MultiGenRest
+{
+    class Alpha
+    {
+        public int code;
+        public override string ToString()
+        {
+            return "Alpa: " + code;
+        }
+    }
+
+    class Bravo : Alpha
+    {
+        public string text;
+        public override string ToString()
+        {
+            return "Bravo: " + code + " и " + text;
+        }
+    }
+
+    class MyClass<X, Y>
+        where X : class, new()
+        where Y : X, new()
+    {
+        public X first;
+        public Y second;
+
+        public MyClass()
+        {
+            first = new X();
+            second = new Y();
+
+
+        }
+
+        public override string ToString()
+        {
+            return "MyClass->|" + first + "|" + second + "|";
+        }
+
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<Alpha, Bravo> obj = new MyClass<Alpha, Bravo>();
+            obj.first.code = 100;
+            obj.second.code = 200;
+            obj.second.text = "text";
+            Console.WriteLine(obj);
+        }
+    }
+}
