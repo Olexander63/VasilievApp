df80262e40fe1fc6cdd135633bf528f3e35c1933
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-14 17:34:05 +0200

    DateTimeDemo

---
+++ NameSpaceDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace First
+{
+    class MyClass
+    {
+        public int num;
+
+        public MyClass(int n)
+        {
+            num = n;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Клас из First: "+ num);
+        }
+    }
+
+}
+
+namespace Second
+{
+    class MyClass
+    {
+        public string txt;
+
+        public MyClass(string t)
+        {
+            txt = t;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Клас из  Seceond: "+ txt);
+        }
+    }
+
+    class NameSpace
+    {
+        static void Main()
+        {
+            First.MyClass A = new First.MyClass(123);
+            Second.MyClass B = new MyClass("text");
+            A.show();
+            B.show();
+        }
+    }
+}
---
+++ UsingNameSpaceDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using  System;
+
+using  First;
+using myclass = Second.MyClass;
+
+namespace First
+{
+
+    class MyClass
+    {
+        public int num;
+
+        public MyClass(int n)
+        {
+            num = n;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Класс из First: " + num);
+        }
+
+    }
+}
+
+namespace Second
+{
+    class MyClass
+    {
+        public string txt;
+
+        public MyClass(string t)
+        {
+            txt = t;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Клас из Second: "+ txt);
+        }
+    }
+}
+namespace UsingNameSpaceDemo
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass A = new MyClass(123);
+            myclass B = new myclass("text");
+            A.show();
+            B.show();
+        }
+    }
+}
