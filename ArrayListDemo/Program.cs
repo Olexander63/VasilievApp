a5d7a276f70df4d672855cd7509f2417ff8f9c8f
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-14 22:45:30 +0200

    StackQueue

---
+++ ArrayListDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListDemo
+{
+    internal class ArrayListColls
+    {
+        public static void Main(string[] args)
+        {
+            int n = 10;
+            ArrayList fibs = new ArrayList();
+            fibs.Add(1);
+            fibs.Add(1);
+            for (int k = 2; k <=n ; k++)
+            {
+                fibs.Add((int) fibs[fibs.Count - 1] + (int) fibs[fibs.Count - 2]);
+            }
+
+            foreach (object obj in fibs)
+            {
+                Console.Write("|"+ obj);
+            }
+
+            Console.WriteLine("|");
+            fibs.Remove(1);
+            fibs.Remove(5);
+            fibs.Insert(0,100);
+            fibs.Reverse();
+            int[] nums = (int[]) fibs.ToArray(typeof(int));
+            for (int k = 0; k < nums.Length; k++)
+            {
+                Console.Write("|"+nums[k]);
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
---
+++ StackQueue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace StackQueue
+{
+    class stackQueueDemo
+    {
+        static void show(ICollection s)
+        {
+            Console.WriteLine("Содержимое коллекции:");
+            foreach (object obj in s)
+            {
+                Console.Write("|" + obj);
+            }
+
+            Console.WriteLine("|");
+        }
+        static void Main()
+        {
+            Stack mystack = new Stack();
+            mystack.Push("Первый");
+            mystack.Push("Второй");
+            mystack.Push("Третий");
+            show(mystack);
+            Console.WriteLine("Прочитано :"+ mystack.Peek());
+            show(mystack);
+            Console.WriteLine("Извлечено:"+ mystack.Pop());
+            show(mystack);
+            Queue myqueue = new Queue();
+            myqueue.Enqueue("Один");
+            myqueue.Enqueue("Два");
+            myqueue.Enqueue("Три");
+            show(myqueue);
+            Console.WriteLine("Прочитано : "+ myqueue.Peek());
+            show(myqueue);
+            Console.WriteLine("Извлечено :"+myqueue.Dequeue());
+            show(myqueue);
+        }
+    }
+}
