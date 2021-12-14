a5d0a33df10216965411492aa3d66295bb5f930d
Oleksandr Omelchenko <olexander63@gmail.com>
2021-12-14 19:49:40 +0200

    CopyToDemo

---
+++ CopyToDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using  System;
+using  System.IO;
+
+namespace CopyToDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string f = "C:/Users/Oleksandr/Pictures/csharp/bear.png";
+            string F = "C:/Users/Oleksandr/Pictures/csharp/Books/animal.png";
+            try
+            {
+                FileStream fin = new FileStream(f, FileMode.Open);
+                Console.WriteLine("Исходный файл: \"{0}\"",f);
+                FileStream fout = new FileStream(F, FileMode.Create);
+                Console.WriteLine("Новый файл: : \"{0}\"",F);
+                fin.CopyTo(fout);
+                fout.Close();
+                fin.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Произошла ошибка!");
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine("Копирование успешно завершено");
+        }
+    }
+}
